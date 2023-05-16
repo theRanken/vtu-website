a9309d7720d9abebc657a5bf6e89615b9c958323
ranken.dev <oshiobugieagwegiokhe@gmail.com>
2023-05-16 16:01:27 +0100

    getdata edit

--- assets/getdata.php
+++ assets/getdata.php
@@ -3,13 +3,14 @@ include '../assets/accounttype.php';
 require_once '../core/conn.php';
 if (isset($_POST['network'])) {
 	$val=$_POST['network'];
-	$sql = "SELECT * FROM plans WHERE network='$val'"; $result = $con->query($sql); if ($result->num_rows > 0) {
-              while($rowd = $result->fetch_assoc())
+	$sql = "SELECT * FROM plans WHERE network='$val'"; $result = $con->query($sql); 
+	if ($result->num_rows > 0) {
+        while($rowd = $result->fetch_assoc())
            $plans .="<option data=\"{$rowd["name"]}\" planid=\"{$rowd["customid"]}\"  plantype=\"{$rowd["type"]}\" value=\"{$rowd["$accounttype"]}\" planprice=\"{$rowd["$accounttype"]}\">{$rowd["name"]} => ₦".number_format($rowd["price"],2)." </option>";
-       ?>
-           <option value="">Select Price</option>
+?>
+    		<option value="">Select Price</option>
             <?php echo $plans; ?> 
-	<?php
+<?php
 	}else{
 
 		echo '<option value="">THERE IS NO DATA PLAN FOR THIS NETWORK</option>';
